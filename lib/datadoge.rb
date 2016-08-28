acf1b6862fbf6cebd55aa0f38618fc19abdbf52f
Cédric Félizard <cedric@felizard.fr>
2016-08-28 14:17:14 +0000

    Track more metrics

--- lib/datadoge.rb
+++ lib/datadoge.rb
@@ -19,34 +19,72 @@ module Datadoge
         event = ActiveSupport::Notifications::Event.new(*args)
         payload = event.payload
 
-        controller = "controller:#{payload.fetch(:controller).underscore}"
-        action = "action:#{payload.fetch(:action)}"
-        controller_action = "controller_action:#{payload.fetch(:controller).underscore}_#{payload.fetch(:action)}"
-        format = "format:#{payload.fetch(:format, 'all')}"
-        format = "format:all" if format == "format:*/*"
-        tags = [controller, action, controller_action, format] + Datadoge.configuration.tags
+        controller = payload.fetch(:controller).underscore
+        action = payload.fetch(:action)
+        controller_action = "#{controller}.#{action}"
+        method = payload.fetch(:method)
+        format = payload[:format] || "all"
+        format = "all" if format == "*/*"
+        status = payload[:status].to_s
 
-        ActiveSupport::Notifications.instrument "datadoge", action: :timing, tags: tags, measurement: "request.duration", value: event.duration
+        tags = Datadoge.configuration.tags + [
+          "controller:#{controller}",
+          "action:#{action}",
+          "controller_action:#{controller_action}",
+          "method:#{method}",
+          "format:#{format}"
+        ]
+        tags << "status:#{status}" if status.present?
 
-        if (db_runtime = payload[:db_runtime])
-          ActiveSupport::Notifications.instrument "datadoge", action: :timing, tags: tags, measurement: "request.db_runtime", value: db_runtime
+        ActiveSupport::Notifications.instrument "datadoge", action: :increment, tags: tags, measurement: "request.total"
+        ActiveSupport::Notifications.instrument "datadoge", action: :histogram, tags: tags, measurement: "request.duration", value: event.duration
+        ActiveSupport::Notifications.instrument "datadoge", action: :increment, tags: tags, measurement: "request.slow" if event.duration > 200.0
+        ActiveSupport::Notifications.instrument "datadoge", action: :increment, tags: tags, measurement: "request.method.#{method}"
+
+        if status.present?
+          ActiveSupport::Notifications.instrument "datadoge", action: :increment, tags: tags, measurement: "request.status.#{status}"
+          ActiveSupport::Notifications.instrument "datadoge", action: :increment, tags: tags, measurement: "request.status.#{status[0]}xx"
+          ActiveSupport::Notifications.instrument "datadoge", action: :histogram, tags: tags, measurement: "request.status.#{status}.duration", value: event.duration
+          ActiveSupport::Notifications.instrument "datadoge", action: :histogram, tags: tags, measurement: "request.status.#{status[0]}xx.duration", value: event.duration
         end
 
-        if (view_runtime = payload[:view_runtime])
-          ActiveSupport::Notifications.instrument "datadoge", action: :timing, tags: tags, measurement: "request.view_runtime", value: view_runtime
+        if payload[:exception]
+          ActiveSupport::Notifications.instrument "datadoge", action: :increment, tags: tags, measurement: "request.exception"
+        else
+          ActiveSupport::Notifications.instrument "datadoge", action: :histogram, tags: tags, measurement: "request.db_runtime", value: payload[:db_runtime] || 0
+          ActiveSupport::Notifications.instrument "datadoge", action: :histogram, tags: tags, measurement: "request.view_runtime", value: payload[:view_runtime] || 0
         end
+      end
 
-        method_path = "#{payload.fetch(:method)}_#{payload.fetch(:path)}"
-        ActiveSupport::Notifications.instrument "datadoge", action: :increment, tags: tags, measurement: "request.method_path.#{method_path}"
+      %w(read generate fetch_hit write delete).each do |metric|
+        ActiveSupport::Notifications.subscribe "cache_#{metric}.active_support" do |*args|
+          event = ActiveSupport::Notifications::Event.new(*args)
 
-        status = payload.fetch(:status, 500)
-        ActiveSupport::Notifications.instrument "datadoge", action: :increment, tags: tags, measurement: "request.status.#{status}"
+          tags = Datadoge.configuration.tags
+
+          ActiveSupport::Notifications.instrument "datadoge", action: :increment, tags: tags, measurement: "cache.total"
+          ActiveSupport::Notifications.instrument "datadoge", action: :increment, tags: tags, measurement: "cache.#{metric}"
+          ActiveSupport::Notifications.instrument "datadoge", action: :histogram, tags: tags, measurement: "cache.#{metric}.duration", value: event.duration
+        end
       end
 
       ActiveSupport::Notifications.subscribe("deliver.action_mailer") do
         tags = Datadoge.configuration.tags
 
-        ActiveSupport::Notifications.instrument "datadoge", action: :increment, tags: tags, measurement: "mailer.deliver"
+        ActiveSupport::Notifications.instrument "datadoge", action: :increment, tags: tags, measurement: "mailer.deliver.total"
+      end
+
+      ActiveSupport::Notifications.subscribe("sql.active_record") do |*args|
+        event = ActiveSupport::Notifications::Event.new(*args)
+        payload = event.payload
+
+        query = payload.fetch(:sql).strip
+        command = query.match(/\A(\w+) /)[1]
+
+        tags = Datadoge.configuration.tags
+
+        ActiveSupport::Notifications.instrument "datadoge", action: :increment, tags: tags, measurement: "sql.total"
+        ActiveSupport::Notifications.instrument "datadoge", action: :increment, tags: tags, measurement: "sql.#{command}" if command.present?
       end
 
       ActiveSupport::Notifications.subscribe("datadoge") do |_name, _started, _finished, _unique_id, data|
@@ -59,11 +97,10 @@ module Datadoge
         tags = data.fetch(:tags)
         action = data.fetch(:action)
 
-        if action == :increment
-          $statsd.increment key_name, tags: tags
-        else
-          value = data.fetch(:value)
-          $statsd.histogram key_name, value, tags: tags
+        case action
+        when :increment then $statsd.increment key_name, tags: tags
+        when :histogram then $statsd.histogram key_name, data.fetch(:value), tags: tags
+        else raise "Unknown action: #{action}"
         end
       end
     end
